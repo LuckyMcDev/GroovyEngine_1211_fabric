abf24638ce300c890a4465f23bd630ebfc943804
LuckyMcDev <lucky.dev@myyahoo.com>
2025-06-12 16:14:49 +0200

    Update Editor to use Screen

--- src/main/java/io/github/luckymcdev/api/editor/MainEditorImGui.java
+++ src/main/java/io/github/luckymcdev/api/editor/MainEditorImGui.java
@@ -1,10 +1,12 @@
 package io.github.luckymcdev.api.editor;
 
+import imgui.flag.ImGuiDockNodeFlags;
 import imgui.flag.ImGuiWindowFlags;
 import io.github.luckymcdev.api.scripting.gui.GuiRegistry;
 import io.github.luckymcdev.impl.imgui.ImGuiImpl;
 import imgui.ImGui;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Window;
 
 public class MainEditorImGui {
     private static float menuBarHeight = 0;
@@ -15,14 +17,10 @@ public class MainEditorImGui {
     public static boolean showBlockBrowser = true;
 
     public static void render() {
-        if (EditorState.open) {
-            MinecraftClient.getInstance().mouse.unlockCursor();
-        } else {
-            MinecraftClient.getInstance().mouse.lockCursor();
-        }
-
         ImGuiImpl.draw(io -> {
             ImGui.pushFont(ImGuiImpl.getDefaultFont());
+
+            renderDockSpace();
 
             renderMenuBar();
 
@@ -37,10 +35,31 @@ public class MainEditorImGui {
         });
     }
 
-    private static void renderMenuBar() {
-        ImGui.setNextWindowPos(0, 0);
-        ImGui.setNextWindowSize(ImGui.getIO().getDisplaySizeX(), 0);
+    private static void renderDockSpace() {
 
+        Window window = MinecraftClient.getInstance().getWindow();
+
+        int flags = ImGuiWindowFlags.NoTitleBar |
+                ImGuiWindowFlags.NoResize |
+                ImGuiWindowFlags.NoMove |
+                ImGuiWindowFlags.NoScrollbar |
+                ImGuiWindowFlags.NoCollapse |
+                ImGuiWindowFlags.MenuBar |
+                ImGuiWindowFlags.NoBackground;
+
+        ImGui.setNextWindowPos(0,0);
+        ImGui.setNextWindowSize(window.getWidth(), window.getHeight());
+        ImGui.begin("DOCKSPACE", flags);
+
+        // Dockspace ID
+        int dockspaceId = ImGui.getID("GroovyEngineDockspace");
+
+        ImGui.dockSpace(dockspaceId, 0.0f, 0.0f, ImGuiDockNodeFlags.PassthruCentralNode);
+
+        ImGui.end();
+    }
+
+    private static void renderMenuBar() {
         int flags = ImGuiWindowFlags.NoTitleBar |
                 ImGuiWindowFlags.NoResize |
                 ImGuiWindowFlags.NoMove |
---
+++ src/main/java/io/github/luckymcdev/api/editor/MainEditorScreen.java
@@ -0,0 +1,24 @@
+package io.github.luckymcdev.api.editor;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class MainEditorScreen extends Screen {
+
+    public MainEditorScreen(Text title) {
+        super(title);
+    }
+
+    @Override
+    public void render(DrawContext ctx, int mouseX, int mouseY, float delta) {
+
+        MainEditorImGui.render();
+
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
+    }
+}

16030e9d5e5e7a133b97120dcb48ec06681de4fd
LuckyMcDev <lucky.dev@myyahoo.com>
2025-06-23 18:57:43 +0200

    update gitignore and fix imgui based error upsie

--- src/main/java/io/github/luckymcdev/groovyengine/editor/core/MainEditorImGui.java
+++ src/main/java/io/github/luckymcdev/groovyengine/editor/core/MainEditorImGui.java
@@ -3,7 +3,7 @@ package io.github.luckymcdev.groovyengine.editor.core;
 import io.github.luckymcdev.groovyengine.editor.windows.BlockDisplayBrowserWindow;
 import io.github.luckymcdev.groovyengine.editor.windows.ConsoleWindow;
 import io.github.luckymcdev.groovyengine.editor.windows.DocsWindow;
-import io.github.luckymcdev.groovyengine.editor.windows.EditorWindow;       // <-- added
+import io.github.luckymcdev.groovyengine.editor.windows.EditorWindow;
 import io.github.luckymcdev.groovyengine.editor.windows.ShaderWindow;
 import io.github.luckymcdev.groovyengine.scripting.gui.GuiRegistry;
 import io.github.luckymcdev.groovyengine.imgui.ImGuiImpl;
@@ -15,13 +15,11 @@ import net.fabricmc.api.Environment;
 @Environment(EnvType.CLIENT)
 public class MainEditorImGui {
 
-    public static boolean showGroovyEngineModellingGroup = false;
-
     public static boolean showConsole = true;
     public static boolean showDocs = false;
     public static boolean showBlockBrowser = false;
     public static boolean showShaderEditor = false;
-    public static boolean showEditorWindow = false; // new toggle for EditorWindow
+    public static boolean showEditorWindow = false;
 
     public static void render() {
         ImGuiImpl.draw(io -> {
@@ -31,15 +29,12 @@ public class MainEditorImGui {
 
             GuiRegistry.renderAll();
 
-            if (showGroovyEngineModellingGroup) {
-                renderGroovyEngineModellingGroup();
-            } else {
-                if (showConsole) ConsoleWindow.render();
-                if (showDocs) DocsWindow.render();
-                if (showBlockBrowser) BlockDisplayBrowserWindow.render();
-                if (showShaderEditor) ShaderWindow.render();
-                if (showEditorWindow) EditorWindow.draw();
-            }
+            if (showConsole) ConsoleWindow.render();
+            if (showDocs) DocsWindow.render();
+            if (showBlockBrowser) BlockDisplayBrowserWindow.render();
+            if (showShaderEditor) ShaderWindow.render();
+            if (showEditorWindow) EditorWindow.draw();
+
 
             ImGui.popFont();
         });
@@ -53,32 +48,15 @@ public class MainEditorImGui {
                 if (ImGui.menuItem("Script Editor", "", showEditorWindow)) showEditorWindow = !showEditorWindow;
                 ImGui.separator();
                 if (ImGui.menuItem("Documentation", "", showDocs)) showDocs = !showDocs;
-                //if (ImGui.menuItem("Block Browser", "", showBlockBrowser)) showBlockBrowser = !showBlockBrowser;
                 ImGui.separator();
                 if (ImGui.menuItem("Shader Editor", "", showShaderEditor)) showShaderEditor = !showShaderEditor;
-                ImGui.endMenu();
-            }
-
-            // New menu for Ge Modelling
-            if (ImGui.beginMenu("GroovyEngine Modelling")) {
-                if (ImGui.menuItem("Open GroovyEngine Modelling Screen")) {
-                    // Close all other windows
-                    showConsole = false;
-                    showDocs = false;
-                    showBlockBrowser = false;
-                    showShaderEditor = false;
-                    showEditorWindow = false;
-
-                    // Open only modelling group
-                    showGroovyEngineModellingGroup = true;
-                }
                 ImGui.endMenu();
             }
 
             if (ImGui.beginMenu("View")) {
                 if (ImGui.menuItem("Reset Layout")) {
                     showConsole = true;
-                    showEditorWindow = true;   // reset EditorWindow visible on reset
+                    showEditorWindow = true;
                     showDocs = true;
                     showShaderEditor = true;
                 }
@@ -87,10 +65,6 @@ public class MainEditorImGui {
 
             ImGui.endMainMenuBar();
         }
-    }
-
-    private static void renderGroovyEngineModellingGroup() {
-        GeModellingImGui.render();
     }
 
 }
